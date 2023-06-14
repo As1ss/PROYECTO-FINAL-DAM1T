8880659ace14a394129387e9fbb0413d9e6ce65a
As1ss <alexis.lopbri@educa.jcyl.es>
2023-06-14 04:03:17 +0200

    Realizado lógica de agregado historial en fichero, falta fixear el nombre de los clientes sobre los ficheros

--- PROYECTO_FINAL/src/controlador/ControladorVentanaCliente.java
+++ PROYECTO_FINAL/src/controlador/ControladorVentanaCliente.java
@@ -2,7 +2,12 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 
 import javax.swing.JOptionPane;
@@ -11,103 +16,117 @@ import modelo.Libro;
 import modelo.OperacionesAdministrador;
 import modelo.OperacionesCliente;
 import modelo.OperacionesUsuario;
+import modelo.RegistroFechas;
 import modelo.Usuario;
 import vista.VentanaCliente;
 
 public class ControladorVentanaCliente implements ActionListener {
 
-	VentanaCliente ventanaCli;
-	OperacionesCliente operacionesCli;
-	OperacionesAdministrador operacionesAdmin;
-	OperacionesUsuario operacionesUsu;
-	private Usuario usu;
+    VentanaCliente ventanaCli;
+    OperacionesCliente operacionesCli;
+    OperacionesAdministrador operacionesAdmin;
+    OperacionesUsuario operacionesUsu;
+    private Usuario usu;
+    private String archivoRegistros = "src/documentos/historial.txt";
 
-	public ControladorVentanaCliente(Usuario usu) {
-		this.usu = usu;
-		System.out.println(usu.getAlias());
-		operacionesUsu = new OperacionesUsuario();
-		operacionesUsu.cargarUsuarios();
-		operacionesAdmin = new OperacionesAdministrador();
-		operacionesCli = new OperacionesCliente(operacionesAdmin);
-		ventanaCli = new VentanaCliente();
-		ventanaCli.setTitle("cliente");
-		ventanaCli.getBtnDevolucion().addActionListener(this);
-		ventanaCli.getBtnDevolver().addActionListener(this);
-		ventanaCli.getBtnPedido().addActionListener(this);
-		ventanaCli.getBtnPedir().addActionListener(this);
-		ventanaCli.setVisible(true);
-		agregarLibrosPedido();
-		agregarLibrosDevolucion();
-	}
+    public ControladorVentanaCliente(Usuario usu) {
+        this.usu = usu;
+        operacionesUsu = new OperacionesUsuario();
+        operacionesUsu.cargarUsuarios();
+        operacionesAdmin = new OperacionesAdministrador();
+        operacionesCli = new OperacionesCliente(operacionesAdmin);
+        ventanaCli = new VentanaCliente();
+        ventanaCli.setTitle("cliente");
+        ventanaCli.getBtnDevolucion().addActionListener(this);
+        ventanaCli.getBtnDevolver().addActionListener(this);
+        ventanaCli.getBtnPedido().addActionListener(this);
+        ventanaCli.getBtnPedir().addActionListener(this);
+        ventanaCli.setVisible(true);
+        agregarLibrosPedido();
+        agregarLibrosDevolucion();
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == ventanaCli.getBtnDevolucion()) {
+            ventanaCli.getCardLayout().show(ventanaCli.getPanelGeneral(), "panelDevolucion");
+            agregarLibrosDevolucion();
+        }
+        if (e.getSource() == ventanaCli.getBtnPedido()) {
+            ventanaCli.getCardLayout().show(ventanaCli.getPanelGeneral(), "panelPedido");
+        }
+        if (e.getSource() == ventanaCli.getBtnPedir()) {
+            String valor = (String) ventanaCli.getListPedido().getSelectedValue();
+            if (usu.getDisponibilidadPrestamo()) {
+                for (Entry<Integer, Libro> entry : operacionesAdmin.getListaLibros().entrySet()) {
+                    Libro libro = entry.getValue();
+                    if (libro.getTitulo().equals(valor)) {
+                        operacionesCli.cogerLibro(entry.getKey(), usu);
+                        operacionesUsu.guardarUsuarios();
+                        JOptionPane.showMessageDialog(ventanaCli, "Libro " + libro.getTitulo() + " pedido.");
 
-		if (e.getSource() == ventanaCli.getBtnDevolucion()) {
+                        // Guardar registro en el archivo de historial
+                        guardarRegistro(usu.getNombre(), libro.getTitulo(), LocalDate.now().toString(), "");
 
-			ventanaCli.getCardLayout().show(ventanaCli.getPanelGeneral(), "panelDevolucion");
-			agregarLibrosDevolucion();
+                        break; // Se encontró el libro, se sale del bucle
+                    }
+                }
+            } else {
+                JOptionPane.showMessageDialog(ventanaCli, "No puedes coger el libro ya que posees uno actualmente.");
+            }
+        }
+        if (e.getSource() == ventanaCli.getBtnDevolver()) {
+            String valor = (String) ventanaCli.getListDevolucion().getSelectedValue();
+            if (!valor.equals("No tienes libros en tu posesión")) {
+                for (Entry<Integer, Libro> entry : operacionesAdmin.getListaLibros().entrySet()) {
+                    Libro libro = entry.getValue();
+                    if (libro.getTitulo().equals(valor)) {
+                        operacionesCli.devolverLibro(entry.getKey(), usu);
+                        operacionesUsu.guardarUsuarios();
+                        JOptionPane.showMessageDialog(ventanaCli, "Libro " + libro.getTitulo() + " devuelto.");
 
-		}
-		if (e.getSource() == ventanaCli.getBtnPedido()) {
+                        // Guardar registro en el archivo de historial
+                        guardarRegistro(usu.getNombre(), libro.getTitulo(), "", LocalDate.now().toString());
 
-			ventanaCli.getCardLayout().show(ventanaCli.getPanelGeneral(), "panelPedido");
-		}
-		if (e.getSource() == ventanaCli.getBtnPedir()) {
-			String valor = (String) ventanaCli.getListPedido().getSelectedValue();
-			if (usu.getDisponibilidadPrestamo()) {
-				for (Entry<Integer, Libro> entry : operacionesAdmin.getListaLibros().entrySet()) {
-					Libro libro = entry.getValue();
-					if (libro.getTitulo().equals(valor)) {
-						operacionesCli.cogerLibro(entry.getKey(), usu);
-						operacionesUsu.guardarUsuarios();
-						JOptionPane.showMessageDialog(ventanaCli, "Libro " + libro.getTitulo() + " pedido.");
-						System.out.println(usu.getDisponibilidadPrestamo());
+                        break; // Se encontró el libro, se sale del bucle
+                    }
+                }
+            } else {
+                JOptionPane.showMessageDialog(ventanaCli, "No tienes libros para devolver.");
+            }
+        }
+    }
 
-					}
-				}
-			} else {
-				JOptionPane.showMessageDialog(ventanaCli, "No puedes coger el libro ya que posees uno actualmente.");
-			}
-			System.out.println(usu.getLibroPrestado().getTitulo());
+    public void agregarLibrosPedido() {
+        ventanaCli.getListModelPedido().removeAllElements();
+        for (Libro titulo : operacionesAdmin.getListaLibros().values()) {
+            ventanaCli.getListModelPedido().addElement(titulo.getTitulo());
+        }
+    }
 
-		}
-		if (e.getSource() == ventanaCli.getBtnDevolver()) {
-			String valor = (String) ventanaCli.getListDevolucion().getSelectedValue();
-			if (!valor.equals("No tienes libros en tu posesión")) {
-				for (Entry<Integer, Libro> entry : operacionesAdmin.getListaLibros().entrySet()) {
-					Libro libro = entry.getValue();
-					if (libro.getTitulo().equals(valor)) {
-						operacionesCli.devolverLibro(entry.getKey(), usu);
-						operacionesUsu.guardarUsuarios();
-						JOptionPane.showMessageDialog(ventanaCli, "Libro " + libro.getTitulo() + " devuelto.");
-						System.out.println(usu.getDisponibilidadPrestamo());
-						break; // Se encontró el libro, se sale del bucle
-					}
-				}
-			} else {
-				JOptionPane.showMessageDialog(ventanaCli, "No tienes libros para devolver.");
-			}
-			System.out.println(usu.getLibroPrestado());
+    public void agregarLibrosDevolucion() {
+        ventanaCli.getListModelDevolucion().removeAllElements();
+        if (usu.getLibroPrestado() != null) {
+            ventanaCli.getListModelDevolucion().addElement(usu.getLibroPrestado().getTitulo());
+        } else {
+            ventanaCli.getListModelDevolucion().addElement("No tienes libros en tu posesión");
+        }
+    }
 
-		}
-	}
-
-	public void agregarLibrosPedido() {
-		ventanaCli.getListModelPedido().removeAllElements();
-		for (Libro titulo : operacionesAdmin.getListaLibros().values()) {
-			ventanaCli.getListModelPedido().addElement(titulo.getTitulo());
-		}
-	}
-
-	public void agregarLibrosDevolucion() {
-	    ventanaCli.getListModelDevolucion().removeAllElements();
-	    if (usu.getLibroPrestado() != null) {
-	        ventanaCli.getListModelDevolucion().addElement(usu.getLibroPrestado().getTitulo());
-	        System.out.println(usu.getLibroPrestado().getTitulo());
-	    } else {
-	        ventanaCli.getListModelDevolucion().addElement("No tienes libros en tu posesión");
-	    }
-	}
-
+    public void guardarRegistro(String cliente, String tituloLibro, String fechaPedido, String fechaDevolucion) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivoRegistros, true))) {
+            writer.write("Cliente: " + cliente);
+            writer.newLine();
+            writer.write("Título del libro: " + tituloLibro);
+            writer.newLine();
+            writer.write("Fecha de pedido: " + fechaPedido);
+            writer.newLine();
+            writer.write("Fecha de devolución: " + fechaDevolucion);
+            writer.newLine();
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Error al guardar el registro en el archivo de historial: " + e.getMessage());
+        }
+    }
 }
--- PROYECTO_FINAL/src/modelo/RegistroFechas.java
+++ PROYECTO_FINAL/src/modelo/RegistroFechas.java
@@ -1,108 +1,62 @@
 package modelo;
 
-import java.time.LocalDate;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
-import java.io.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+public class RegistroFechas<T> {
+	private T fechaPedido;
+	private T fechaDevolucion;
+	private String cliente;
+	private String tituloLibro;
 
-public class RegistroFechas<C, L> {
-	private C elemento;
-	private L elemento2;
-	private LocalDate fechaPedido;
-	private LocalDate fechaDevolucion;
-
-	public RegistroFechas(C elemento, L elementodos, LocalDate fechaPedido) {
-		this.elemento = elemento;
+	public RegistroFechas(String cliente, String tituloLibro, T fechaPedido, T fechaDevolucion) {
+		this.cliente = cliente;
+		this.tituloLibro = tituloLibro;
 		this.fechaPedido = fechaPedido;
-		this.elemento2 = elementodos;
-	}
-
-	public C getElemento() {
-		return elemento;
-	}
-
-	public LocalDate getFechaPedido() {
-		return fechaPedido;
-	}
-
-	public LocalDate getFechaDevolucion() {
-		return fechaDevolucion;
-	}
-
-	public void setFechaDevolucion(LocalDate fechaDevolucion) {
 		this.fechaDevolucion = fechaDevolucion;
 	}
 
-	public L getElemento2() {
-		return elemento2;
+	public T getFechaPedido() {
+		return fechaPedido;
 	}
 
-	public void setElemento2(L elemento2) {
-		this.elemento2 = elemento2;
-	}
-
-	public void setElemento(C elemento) {
-		this.elemento = elemento;
-	}
-
-	public void setFechaPedido(LocalDate fechaPedido) {
+	public void setFechaPedido(T fechaPedido) {
 		this.fechaPedido = fechaPedido;
 	}
 
-	public static List<RegistroFechas<String, String>> cargarRegistros(String archivo) {
-		List<RegistroFechas<String, String>> listaRegistros = new ArrayList<>();
-
-		try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-			String linea;
-
-			while ((linea = reader.readLine()) != null) {
-				String cliente = linea.substring(linea.indexOf(":") + 2);
-
-				linea = reader.readLine();
-				String libro = linea.substring(linea.indexOf(":") + 2);
-
-				linea = reader.readLine();
-				LocalDate fechaPedido = LocalDate.parse(linea.substring(linea.indexOf(":") + 2));
-
-				linea = reader.readLine();
-				LocalDate fechaDevolucion = LocalDate.parse(linea.substring(linea.indexOf(":") + 2));
-
-				RegistroFechas<String, String> registro = new RegistroFechas<>(cliente, libro, fechaPedido);
-				registro.setFechaDevolucion(fechaDevolucion);
-				listaRegistros.add(registro);
-
-				// Leer la línea en blanco que separa los registros
-				reader.readLine();
-			}
-
-			System.out.println("Se han cargado los registros del archivo '" + archivo + "'.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return listaRegistros;
+	public T getFechaDevolucion() {
+		return fechaDevolucion;
 	}
 
-	public static void guardarRegistros(List<RegistroFechas<String, String>> listaRegistros, String archivo) {
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo))) {
-			for (RegistroFechas<String, String> registro : listaRegistros) {
-				writer.write("");
-				writer.write("Cliente: " + registro.getElemento());
-				writer.newLine();
-				writer.write("Libro: " + registro.getElemento2());
-				writer.newLine();
-				writer.write("Fecha de pedido: " + registro.getFechaPedido());
-				writer.newLine();
-				writer.write("Fecha de devolución: " + registro.getFechaDevolucion());
-				writer.newLine();
-				writer.newLine(); // Línea en blanco para separar los registros
-			}
+	public void setFechaDevolucion(T fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
 
-			System.out.println("Se han guardado los registros en el archivo '" + archivo + "'.");
+	public String getCliente() {
+		return cliente;
+	}
+
+	public String getTituloLibro() {
+		return tituloLibro;
+	}
+
+	public void guardarInformacion() {
+		String nombreArchivo = cliente + "_" + tituloLibro + ".txt";
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+			writer.write("Cliente: " + cliente);
+			writer.newLine();
+			writer.write("Título del libro: " + tituloLibro);
+			writer.newLine();
+			writer.write("Fecha de pedido: " + fechaPedido.toString());
+			writer.newLine();
+			writer.write("Fecha de devolución: " + fechaDevolucion.toString());
+			writer.newLine();
+			System.out.println("La información se ha guardado correctamente en el archivo: " + nombreArchivo);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Error al guardar la información en el archivo: " + e.getMessage());
 		}
 	}
 }
